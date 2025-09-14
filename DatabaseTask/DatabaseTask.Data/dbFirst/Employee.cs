a95db920c331ffd3ac61a4f921b131297aafeef1
Siim <Siim@DESKTOP-6089TAB>
2025-09-14 20:19:32 +0300

    init2

---
+++ DatabaseTask/DatabaseTask.Data/dbFirst/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DatabaseTask.Data.dbFirst;
+
+public partial class Employee
+{
+    public Guid Id { get; set; }
+
+    public string FirstName { get; set; } = null!;
+
+    public string LastName { get; set; } = null!;
+}
