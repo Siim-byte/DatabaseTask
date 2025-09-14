27ca758caa88fde404d666682531abafeb98bdd3
Siim <Siim@DESKTOP-6089TAB>
2025-09-14 20:00:31 +0300

    init

--- DatabaseTask/DatabaseTask.Data/DatabaseTaskDbContext.cs
+++ DatabaseTask/DatabaseTask.Data/DatabaseTaskDbContext.cs
@@ -9,8 +9,6 @@ namespace DatabaseTask.Data
         public DatabaseTaskDbContext(DbContextOptions<DatabaseTaskDbContext> options)
             : base(options) { }
 
-        // näide, kuidas teha, kui lisate domaini alla ühe objekti
-        // migratsioonid peavad tulema siia libary-sse e TARge20.Data alla.
         public DbSet<Employee> Employee { get; set; }
     }
 }
