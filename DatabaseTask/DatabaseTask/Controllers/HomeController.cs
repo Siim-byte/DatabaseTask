4f3c2a91883be8b39715c482176622514d3a1ce0
Opilane TTHK <189960872+Siim-byte@users.noreply.github.com>
2025-10-31 16:15:00 +0200

    tehtud sp

--- DatabaseTask/DatabaseTask/Controllers/HomeController.cs
+++ DatabaseTask/DatabaseTask/Controllers/HomeController.cs
@@ -1,23 +1,51 @@
-﻿using DatabaseTask.Models;
+﻿using DatabaseTask.Data;
+using DatabaseTask.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
 using System.Diagnostics;
 
 namespace DatabaseTask.Controllers
 {
     public class HomeController : Controller
     {
+        public DatabaseTaskDbContext _context;
+        public IConfiguration _confiq;
+
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+
+        public HomeController
+            (
+            ILogger<HomeController> logger,
+            DatabaseTaskDbContext context,
+            IConfiguration config
+            )
         {
             _logger = logger;
+            _context = context;
+            _confiq = config;
         }
 
+
+        [HttpGet]
         public IActionResult Index()
         {
-            return View();
-        }
+            string connectionStr = _confiq.GetConnectionString("DefaultConnection");
 
+            using (SqlConnection con = new SqlConnection(connectionStr))
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = con;
+                cmd.CommandText = "dbo.spGetPatient";
+                cmd.CommandType = System.Data.CommandType.Text;
+                con.Open();
+                SqlDataReader sdr = cmd.ExecuteReader();
+
+
+
+                return View();
+            }
+        }
         public IActionResult Privacy()
         {
             return View();
